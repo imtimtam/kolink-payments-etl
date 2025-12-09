9987c230fae43f96bd14a2bb737cad868cd81cbc
Timothy Tam <imtimtam@gmail.com>
2025-12-08 17:02:31 -0800

    feat: add time logging test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import pandas as pd
 from pathlib import Path
 
@@ -6,6 +7,7 @@ from utils.utils_cms import read_cms_files, clean_cms_files
 years = range(2018, 2024)
 
 for year in years:
+    start_time = datetime.now()
     gen_file = Path(f"./data/general_payments") / f"cms_general_{year}.csv"
     res_file = Path(f"./data/research_payments") / f"cms_research_{year}.csv"
 
@@ -29,7 +31,9 @@ for year in years:
     unified_df.drop_duplicates(inplace=True)
     unified_df.to_csv(export_path, index=False, encoding="utf-8")
 
-    print(f"Year {year} processed and saved to {export_path}")
+    end_time = datetime.now()
+    elapsed_time = end_time - start_time
+    print(f"Year {year} processed and saved to {export_path} (elapsed: {elapsed_time})")
 
 
 
